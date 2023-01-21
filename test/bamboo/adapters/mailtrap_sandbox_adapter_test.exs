c865bc62a2127fddbbcf564bb32585333e6da573
Kalys Osmonov <kalys@osmonov.com>
2023-01-21 13:49:12 +0600

    Add test for sandbox adapter

---
+++ test/bamboo/adapters/mailtrap_sandbox_adapter_test.exs
@@ -0,0 +1,59 @@
+defmodule Bamboo.MailtrapSandboxAdapterTest do
+  use ExUnit.Case
+  import Tesla.Mock
+  alias Bamboo.{Email, Mailer, MailtrapSandboxAdapter}
+
+  @doc false
+  def new_email(to \\ "alice@example.com", subject \\ "Welcome to the app.") do
+    Email.new_email(
+      to: to,
+      from: "bob@example.com",
+      cc: "john@example.com",
+      bcc: "jane@example.com",
+      subject: subject,
+      html_body: "<strong>Thanks for joining!</strong>",
+      text_body: "Thanks for joining!"
+    )
+    |> Mailer.normalize_addresses()
+  end
+
+  def api_configs, do: %{api_token: "api_token", inbox_id: 11111}
+
+  test "happy path" do
+    expected_request_body =
+      Jason.encode!(%{
+        attachments: [],
+        bcc: [%{email: "jane@example.com"}],
+        cc: [%{email: "john@example.com"}],
+        from: %{email: "bob@example.com"},
+        headers: %{},
+        html: "<strong>Thanks for joining!</strong>",
+        subject: "Welcome to the app.",
+        text: "Thanks for joining!",
+        to: [%{email: "alice@example.com"}],
+      })
+
+    mock(fn
+      %{
+        method: :post,
+        body: ^expected_request_body,
+        headers: [
+          {"content-type", "application/json"},
+          {"authorization", "Bearer api_token"}
+        ],
+        url: "https://sandbox.api.mailtrap.io/api/send/11111"} ->
+        json(%{success: true, message_ids: [1]}, status: 200)
+    end)
+
+    assert {:ok, _} = MailtrapSandboxAdapter.deliver(new_email(), api_configs())
+  end
+
+  test "returns error" do
+    mock(fn
+      %{method: :post, url: "https://sandbox.api.mailtrap.io/api/send/11111"} ->
+        json(%{error: "Incorrect API token"}, status: 401)
+    end)
+
+    assert {:error, _} = MailtrapSandboxAdapter.deliver(new_email(), api_configs())
+  end
+end
